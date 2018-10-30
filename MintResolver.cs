68b167fbb6f47e80cc2f183ff78cbfad90e5e663
victoor83 <44602692+victoor83@users.noreply.github.com>
2018-10-30 11:39:32 +0100

    Add files via upload
    constructor added

---
+++ MintResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintApp
+{
+    public class MintResolver
+    {
+        private string _mintPath;
+        public MintResolver(string path)
+        {
+            _mintPath = path;
+        }
+    }
+}
