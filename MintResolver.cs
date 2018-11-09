1166a13c98b3d6721a45939725dd4f952fb64a50
victoor83 <44602692+victoor83@users.noreply.github.com>
2018-11-09 13:50:40 +0100

    update: Print function

--- MintResolver.cs
+++ MintResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,32 @@ namespace MintApp
     public class MintResolver
     {
         private string _mintPath;
+        private string[] _mintArray;
         public MintResolver(string path)
         {
             _mintPath = path;
+            ReadMint();
         }
+
+        public void Resolve()
+        {
+
+        }
+
+        public void Print()
+        {
+            foreach (var line  in _mintArray)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        private void ReadMint()
+        {
+            _mintArray = File.ReadAllLines(_mintPath);
+
+        }
+
+
     }
 }
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+
+namespace MintApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\maze.txt"));
+
+            MintResolver resolver = new MintResolver(path);
+            resolver.Print();
+            resolver.Resolve();
+
+            Console.ReadLine();
+        }
+    }
+}
