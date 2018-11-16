4235f4689c42caba15d640c0a3d67d5715aeed9a
victoor83 <44602692+victoor83@users.noreply.github.com>
2018-11-16 14:23:36 +0100

    developing..

--- MintApp/MintApp/MintResolver.cs
+++ MintApp/MintApp/MintResolver.cs
@@ -12,6 +12,9 @@ namespace MintApp
     {
         private string _mintPath;
         private string[] _mintArray;
+        private List<Position> _visitedPositions = new List<Position>();
+        private Direction _lastDirection;
+        private Direction _currentDirection;
         public MintResolver(string path)
         {
             _mintPath = path;
@@ -22,22 +25,21 @@ namespace MintApp
         {
             Position currentPosition = GetStartPoint();
 
-            bool isEmptyStart = IsLocationEmpty(currentPosition);
-            Direction currentDirection = Direction.Right;
+            _currentDirection = Direction.Right;
 
             int moveCounter = 0;
 
             while (true)
             {
 
-                bool wasMovePossible = Move(currentDirection, ref currentPosition);
+                bool wasMovePossible = Move(ref currentPosition);
 
                 if(currentPosition == GetFinishPoint())
                     return;
 
                 if(!wasMovePossible)
                 {
-                    ChangeDirection(ref currentDirection, currentPosition);
+                    ChangeDirection(currentPosition);
                     moveCounter++;
                 }
 
@@ -49,31 +51,46 @@ namespace MintApp
 
         public void Print()
         {
+            int lineCounter = 0;
+            Console.WriteLine();
             foreach (var line  in _mintArray)
             {
-                Console.WriteLine(line);
+                //Print with line number
+                string lineNr = " ";
+                if (lineCounter.ToString().Length == 1)
+                    lineNr = lineNr + lineCounter;
+                else
+                    lineNr = lineCounter.ToString();
+
+                Console.WriteLine(lineNr + line);
+                lineCounter++;
             }
+
+            Console.WriteLine("Maze Lenght: " + _mintArray[0].Length);
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
 
-        private void ChangeDirection(ref Direction currentDirection, Position currentPosition)
+        private void ChangeDirection(Position currentPosition)
         {
-            switch (currentDirection)
+            switch (_currentDirection)
             {
                 case Direction.Right:
                     //Try to go up
                     if(IsLocationEmpty(GetNextPosition(Direction.Up, currentPosition)))
                     {
-                        currentDirection = Direction.Up;         
+                        _currentDirection = Direction.Up;         
                     }
                     //Try to go left
                     else if (IsLocationEmpty(GetNextPosition(Direction.Left, currentPosition)))
                     {
-                        currentDirection = Direction.Left;     
+                        _currentDirection = Direction.Left;     
                     }
                     //Try to go down
                     else if (IsLocationEmpty(GetNextPosition(Direction.Down, currentPosition)))
                     {
-                        currentDirection = Direction.Down;
+                        _currentDirection = Direction.Down;
                     } else
                     {
                         throw new Exception("No move possible");
@@ -84,17 +101,17 @@ namespace MintApp
                     //Try to go down
                     if (IsLocationEmpty(GetNextPosition(Direction.Down, currentPosition)))
                     {
-                        currentDirection = Direction.Down;
+                        _currentDirection = Direction.Down;
                     }
                     //Try to go up
                     else if (IsLocationEmpty(GetNextPosition(Direction.Up, currentPosition)))
                     {
-                        currentDirection = Direction.Up;
+                        _currentDirection = Direction.Up;
                     }
                     //Try to go right
                     else if (IsLocationEmpty(GetNextPosition(Direction.Right, currentPosition)))
                     {
-                        currentDirection = Direction.Right;        
+                        _currentDirection = Direction.Right;        
                     }
                     else
                     {
@@ -106,17 +123,17 @@ namespace MintApp
                     //Try to go left
                     if (IsLocationEmpty(GetNextPosition(Direction.Left, currentPosition)))
                     {
-                        currentDirection = Direction.Left;
-                    }
-                    //Try to go down
-                    else if (IsLocationEmpty(GetNextPosition(Direction.Down, currentPosition)))
-                    {
-                        currentDirection = Direction.Down;
+                        _currentDirection = Direction.Left;
                     }
                     //Try to go right
                     else if (IsLocationEmpty(GetNextPosition(Direction.Right, currentPosition)))
                     {
-                        currentDirection = Direction.Right;
+                        _currentDirection = Direction.Right;
+                    }
+                    //Try to go down
+                    else if (IsLocationEmpty(GetNextPosition(Direction.Down, currentPosition)))
+                    {
+                        _currentDirection = Direction.Down;
                     }
                     else
                     {
@@ -128,17 +145,17 @@ namespace MintApp
                     //Try to go left
                     if (IsLocationEmpty(GetNextPosition(Direction.Left, currentPosition)))
                     {
-                        currentDirection = Direction.Left;
+                        _currentDirection = Direction.Left;
                     }
                     //Try to go right
                     else if (IsLocationEmpty(GetNextPosition(Direction.Right, currentPosition)))
                     {
-                        currentDirection = Direction.Right;
+                        _currentDirection = Direction.Right;
                     }
                     //Try to go up
                     else if (IsLocationEmpty(GetNextPosition(Direction.Up, currentPosition)))
                     {
-                        currentDirection = Direction.Up;
+                        _currentDirection = Direction.Up;
                     }
                     else
                     {
@@ -148,7 +165,7 @@ namespace MintApp
 
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection, null);
+                    throw new ArgumentOutOfRangeException(nameof(_currentDirection), _currentDirection, null);
             }
         }
 
@@ -184,15 +201,32 @@ namespace MintApp
             return false;
         }
 
-        private bool Move(Direction direction, ref Position currentPosition)
+        private bool Move(ref Position currentPosition)
         {
             string directionSymbol;
             Position newPosition;
-            switch (direction)
+            switch (_currentDirection)
             {
                 case Direction.Left:
-                    newPosition = GetNextPosition(Direction.Left, currentPosition);
+                    
+                    if(PositionAlreadyVisited(currentPosition) && _lastDirection != Direction.Down)
+                    {        
+                        var positionUp =GetNextPosition(Direction.Up, currentPosition);
+                        if(IsLocationEmpty(positionUp))
+                        {
+                            currentPosition = positionUp;
+                            directionSymbol = "^";
+                            Console.Write(directionSymbol);
+                            _lastDirection = Direction.Left;
+                            _currentDirection = Direction.Up;
+                            _visitedPositions.Add(currentPosition);
+                            return true;
+                        }
+                    } 
+                    
+                    newPosition = GetNextPosition(Direction.Left, currentPosition);                   
                     directionSymbol = "<";
+
                     break;
                 case Direction.Right:
                     newPosition  = GetNextPosition(Direction.Right, currentPosition);
@@ -207,7 +241,7 @@ namespace MintApp
                     directionSymbol = "v";
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+                    throw new ArgumentOutOfRangeException(nameof(_currentDirection), _currentDirection, null);
             }
 
             if (!IsLocationEmpty(newPosition))            
@@ -215,10 +249,20 @@ namespace MintApp
 
 
             currentPosition = newPosition;
+            _visitedPositions.Add(currentPosition);
+            _lastDirection = _currentDirection;
 
             Console.Write(directionSymbol);
 
             return true;
+        }
+
+        private bool PositionAlreadyVisited(Position position)
+        {
+            if(_visitedPositions.Contains(position))
+                return true;
+
+            return false;
         }
 
 
